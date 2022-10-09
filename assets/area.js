c52a9557ed036380efedcc3c1d654d1fbbcd5e5b
anandbaraik <anandbaraik2014@gmail.com>
2022-10-09 22:59:12 +0530

    fix:negative value validation added

--- assets/area.js
+++ assets/area.js
@@ -6,11 +6,25 @@ const outputBox = document.querySelector(".outputBox");
 btn.addEventListener("click", checkArea);
 
 function checkArea() {
-    if(baseInput.value && heightInput.value) {
-        calculateArea(Number(baseInput.value), Number(heightInput.value));
-    } else {
-        showMessage("All fields are required.");
+    //check if base is non negative value or not empty
+    if((parseInt(baseInput.value) < 0) || (parseInt(baseInput.value) == 0)) {
+        showMessage('Base value should be grater than 0');
+        return false;
+    } else if(!baseInput.value) {
+        showMessage("Base is required.");
+        return false;
     }
+
+    //check if height is nn negative value or not empty
+    if((parseInt(heightInput.value) < 0) || (parseInt(heightInput.value) == 0)) {
+        showMessage('Height value should be grater than 0');
+        return false;
+    } else if (!heightInput.value) {
+        showMessage("Height is required.");
+        return false;
+    }
+
+    calculateArea(Number(baseInput.value), Number(heightInput.value));
 }
 
 function showMessage(msg) {
@@ -21,3 +35,14 @@ function calculateArea(base, height) {
     let area = 1/2*(base * height);
     showMessage(`The area of the triangle is ${area.toFixed(2)} cm².`);
 }
+
+/*
+* Number() converts the type whereas parseInt parses the value of input.
+* // Parsing
+* parseInt('32px'); // 32
+* parseInt('5e1'); // 5
+*
+* // Convert type
+* Number('32px'); // NaN
+* Number('5e1'); // 50
+*/
